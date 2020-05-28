59c610a58a0a808a0ff7b8335c971f787a4109a3
hslusarek <h.slusarek@pilz.de>
2020-05-28 13:50:57 +0200

    Add '--include'  and '--omit' flag to python-coverage commands

--- cmake/Modules/CodeCoverage.cmake
+++ cmake/Modules/CodeCoverage.cmake
@@ -172,9 +172,9 @@ function(ADD_CODE_COVERAGE)
     add_custom_target(${Coverage_NAME}_py
         COMMAND cp ${PROJECT_BINARY_DIR}/.coverage ${COVERAGE_DIR}/.coverage.nosetests || echo "WARNING: No nosetest coverage!"
         COMMAND python-coverage combine || echo "WARNING: No python coverage to combine!"
-        COMMAND python-coverage report
-        COMMAND python-coverage xml || echo "WARNING: No python xml to output"
-        COMMAND python-coverage html || echo "WARNING: No python html to output"
+        COMMAND python-coverage report --include "*${PROJECT_SOURCE_DIR}*" --omit ${COVERAGE_EXCLUDES}
+        COMMAND python-coverage xml    --include "*${PROJECT_SOURCE_DIR}*" --omit ${COVERAGE_EXCLUDES} || echo "WARNING: No python xml to output"
+        COMMAND python-coverage html   --include "*${PROJECT_SOURCE_DIR}*" --omit ${COVERAGE_EXCLUDES} || echo "WARNING: No python html to output"
         WORKING_DIRECTORY ${COVERAGE_DIR}
         DEPENDS _run_tests_${PROJECT_NAME}
     )
